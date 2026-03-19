a59c09f3fd5d1c106bcaa804abf8ac389a736d0f
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2026-03-19 02:34:52 +0100

    Recap: classes, objects, attributes, methods and self

--- script.py
+++ script.py
@@ -25,10 +25,12 @@ class Owner:
 owner1 = Owner("Danny", "122 Springfield Drive", "888-999")    
 dog1 = Dog("Bruce", "Scottish Terrier", owner1)
 dog1.bark()
+print(dog1.owner.name)
 
 owner2 = Owner("Sally", "122 Springfield Drive", "888-999")    
 dog2 = Dog("Freya", "Greyhound", owner2)
 dog2.bark()
+print(dog2.owner.name)
 
 print(dog1.name +"_"+ dog1.breed+"_" + dog1.owner.name)
 print(dog2.name +"_"+ dog2.breed+"_" + dog2.owner.name)
