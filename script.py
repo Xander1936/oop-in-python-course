8b83ab17d56fe636ff9a2677c98c39cb0c90fa2b
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2026-03-19 01:58:23 +0100

    Creating classes and objects

--- script.py
+++ script.py
@@ -1,8 +1,25 @@
 # Everything in Python is an object
 # Class str -> object name
 # Class int -> object age
-name = "Danny"
-age = 29
+# name = "Danny"
+# age = 29
 
-print(type(name))
-print(type(age))
+# print(name.upper())
+# print(type(age))
+
+class Dog:
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed 
+    
+    def bark(self):
+        print("Whoof whoof")
+        
+dog1 = Dog("Bruce", "Scottish Terrier")
+dog1.bark()
+
+dog2 = Dog("Freya", "Greyhound")
+dog2.bark()
+
+print(dog1.name +"_"+ dog1.breed)
+print(dog2.name +"_"+ dog2.breed)
