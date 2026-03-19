4b481eecbedc850b020ad7823d25e6efa015bba8
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2026-03-19 17:54:55 +0100

    When to make an attribute protected

---
+++ 5-When to make an attribute protected/script.py
@@ -0,0 +1,20 @@
+# Accessing and Modifying Data
+# 1. The traditional way: make the data private and use getter and setters:
+
+class User:
+    def __init__(self, username, email, password):
+        self.username = username
+        # "self._" the "_" here make the attribute protected -> The attribute is internal to the class
+        self._email = email
+        self._password = password
+        
+    def get_email(self):
+        return self._email
+    
+    def clean_email(self):
+        return self._email.lower().strip()
+
+user1 = User("dantheman", "Dan@gmail.com ", "123")
+
+print(user1._email)
+print(user1.clean_email())
