a407c89292cad4e5c77a2fb885207ebc5fb8bffb
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2026-03-19 02:29:24 +0100

    Combining objects

--- script.py
+++ script.py
@@ -8,18 +8,27 @@
 # print(type(age))
 
 class Dog:
-    def __init__(self, name, breed):
+    def __init__(self, name, breed, owner):
         self.name = name
-        self.breed = breed 
+        self.breed = breed
+        self.owner = owner 
     
     def bark(self):
         print("Whoof whoof")
         
-dog1 = Dog("Bruce", "Scottish Terrier")
+class Owner:
+    def __init__(self, name, address, contact_number):
+        self.name = name
+        self.address = address
+        self.phone_number = contact_number
+        
+owner1 = Owner("Danny", "122 Springfield Drive", "888-999")    
+dog1 = Dog("Bruce", "Scottish Terrier", owner1)
 dog1.bark()
 
-dog2 = Dog("Freya", "Greyhound")
+owner2 = Owner("Sally", "122 Springfield Drive", "888-999")    
+dog2 = Dog("Freya", "Greyhound", owner2)
 dog2.bark()
 
-print(dog1.name +"_"+ dog1.breed)
-print(dog2.name +"_"+ dog2.breed)
+print(dog1.name +"_"+ dog1.breed+"_" + dog1.owner.name)
+print(dog2.name +"_"+ dog2.breed+"_" + dog2.owner.name)
