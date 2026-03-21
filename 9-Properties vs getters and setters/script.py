2f2d4b12a66782c7dd2a3dc66f50aa6f64619ed4
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2026-03-21 17:02:12 +0100

    Properties vs creating getters and setters

---
+++ 9-Properties vs getters and setters/script.py
@@ -0,0 +1,27 @@
+# Accessing and Modifying Data
+# 1. The traditional way: make the data private and use getter and setters:
+# Name Mangled variables are private variables or attributes
+# 2. Properties
+
+# from datetime import datetime 
+
+class User:
+    def __init__(self, username, email, password):
+        self.username = username
+        # "self._" the "_" here make the attribute protected -> The attribute is internal to the class and can be access outside the class
+        # 1. First Make the attribute private
+        self._email = email
+        self.password = password
+    # 2. Add the property decorator -> @property   
+    # With @property we don4t worry about to create getter
+    @property
+    # 3. Create a method with the same name with the property we want to manipulate
+    def email(self):
+        print("Email accessed")
+        return self._email
+    
+
+user1 = User("dantheman", "dan@gmail.com", "123")
+# user1.email = "this is not an email" 
+print(user1.email)
+
