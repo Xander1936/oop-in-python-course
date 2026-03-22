f0dda9e525f22881f3e67ee97297cb7191b0ef95
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2026-03-22 19:07:49 +0100

    Part II: OOP PRINCIPLES - ENCAPSULATION

---
+++ 15-Part II_OOP PRINCIPLES - ENCAPSULATION/script.py
@@ -0,0 +1,39 @@
+# Encapsulation: bundleling the data with the attributes called fields and methods or behaviours that operates the data in a single unit called class.
+# It helps to only exposing the necessary functionalities to the outside world.
+
+class BadBankAccount:
+    def __init__(self, balance):
+        self.balance = balance
+        
+# account = BadBankAccount(0.0)
+# account.balance = -1
+# print(account.balance)
+
+# ------------------------------------------------------------------
+class BankAccount:
+    def __init__(self):
+        self._balance = 0.0
+        
+    @property
+    def balance(self):
+        return self._balance
+    
+    def deposit(self, amount):
+        if amount <= 0:
+            raise ValueError("Deposit amount must be positive.")
+        self._balance += amount
+    
+    def withdraw(self, amount):
+        if amount <= 0:
+            raise ValueError("Withdraw amount must be positive.")
+        if amount >= self._balance:
+            raise ValueError("Insufficient funds.")
+        self._balance -= amount
+        
+account = BankAccount()
+# account.balance = -1
+account.deposit(1.99)
+print(account.balance)
+account.withdraw(1)
+print(account.balance)
+account.withdraw(100)
